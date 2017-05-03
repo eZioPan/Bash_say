6e08661e934ae70260866fc3d8e3b83284057eb6
eZio Pan <eziopan@qq.com>
2017-05-03 18:00:50 +0800

    change to full screen step

--- sayDoc.sh
+++ sayDoc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+read -p "Input text file: " infile;
 read -p "Output Audio File Name: " outname;
 if [ -z "${outname}" ]; then
 	outname=audio
 fi;
-inDir=$(dirname $1);
-say -v "Alex" -f $1 -o "${inDir}/${outname}.aiff";
+inDir="$(dirname "$1")";
+say -v "Alex" -f "${infile}" -o "${inDir}/${outname}.aiff";
